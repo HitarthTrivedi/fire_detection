a7bb5e18b86f9559f3b5e5fd2bd91c2e0c3938a8
Hitarth <165199401+HitarthTrivedi@users.noreply.github.com>
2025-02-14 01:57:36 +0530

    Update firedetection 2.py

--- firedetection 2.py
+++ firedetection 2.py
@@ -1,11 +1,18 @@
 import cv2 as cv2
+import smtplib
 
 
 
 fire_cascade = cv2.CascadeClassifier('fire_detection.cml')
 
 cap = cv2.VideoCapture(0)
-
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('*sender_email.com*', '*password*')
+    server.sendmail('sender_email.com', to, content)
+    server.close()
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -16,11 +23,19 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         print("fire is detected")
+        content="FIRE WAS DETECTED AT YOUR PLACE!!! FIRE FIRE FIRE FIRE.FIRE WAS DETECTED AT YOUR PLACE!!! FIRE FIRE FIRE FIRE.FIRE WAS DETECTED AT YOUR PLACE!!! FIRE FIRE FIRE FIRE."
+        to = "recievers_email@gmail.com"
+        sendEmail(to, content)
+        print("Email has been sent")
+
+
 
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 
 
 
